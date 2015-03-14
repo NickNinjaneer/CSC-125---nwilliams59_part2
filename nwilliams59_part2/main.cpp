3fe8ea38028b0af6d6fc451ca553492a0e017ecb
Nick Williams <SoundsCustom@gmail.com>
2015-03-13 20:38:17 -0500

    Initial Commit

---
+++ nwilliams59_part2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  nwilliams59_part2
+//
+//  Created by Nick Williams on 3/13/15.
+//  Copyright (c) 2015 Ninjaneer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
